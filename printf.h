c76fce062450f1984b04ca1636a7a2149be55959
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-13 21:59:27 +0200

    Update and rename ft_printf.h to printf.h

--- printf.h
+++ printf.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef PRINTF_H
+# define PRINTF_H
 
 # include "libft/libft.h"
 # include <stdarg.h>
