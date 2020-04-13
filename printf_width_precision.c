fa3a9d0f7542f2613d32d54f4417abe82f3da67f
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-13 20:51:18 +0200

    Create printf_width_precision.c

---
+++ printf_width_precision.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_2_width_precision.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/05 16:30:34 by slabelle          #+#    #+#             */
+/*   Updated: 2020/04/05 17:00:38 by slabelle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+int			printf_width(const char *format, t_specifiers *specs)
+{
+	
+}
+
+int			printf_precision(const char *format, t_specifiers *specs)
+{
+	
+}
