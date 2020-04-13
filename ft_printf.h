79fadbf53020365336cffd4e6ade9e86803814a1
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-13 21:43:24 +0200

    Create ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/05 16:28:56 by slabelle          #+#    #+#             */
+/*   Updated: 2020/04/05 17:44:22 by slabelle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+# include <unistd.h>
+
+typedef struct		s_flags
+{
+	int				minus;
+	int				zero;
+	int				dot;
+	int				aster_width;
+	int				aster_precis;
+}					t_flags;
+
+typedef struct		s_specifiers
+{
+	int				i;
+	int				j;
+	int				width;
+	int				precision;
+	char			letter;
+}					t_whole_specifier;
+
+int					ft_printf(const char *format, ...);
+
+static t_flags		*printf_init_flags(void);
+static t_specifiers	*printf_init_specs(void);
+int					printf_width_precision(const char *format,
+					t_specifiers *specs);
+
+#endif
