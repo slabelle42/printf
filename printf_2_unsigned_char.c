e829d31798a0ab067b477ba08d34de382d7b1c67
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-14 17:09:28 +0200

    Update printf_2_unsigned_char.c

--- printf_2_unsigned_char.c
+++ printf_2_unsigned_char.c
@@ -19,8 +19,7 @@ int		printf_unsigned_char(char char_ap, t_flags *flags, t_specifiers *specs)
 	printed = 0;
 	if (flags->minus == 1)
 		printed += ft_putchar_fd(char_ap, 1);
-	while ((specs->width)-- > 1)
-		printed += ft_putchar_fd(' ', 1);
+	printed += printf_display_width_precision(specs->width, 1, 0);
 	if (flags->minus == 0)
 		printed += ft_putchar_fd(char_ap, 1);
 	return (printed);
