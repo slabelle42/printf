8fcd66570d6213d76736df648da97c02a7c0eba6
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-15 17:31:57 +0200

    Create printf_2_unsigned_decimal.c

---
+++ printf_2_unsigned_decimal.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_2_unsigned_decimal.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/05 16:31:24 by slabelle          #+#    #+#             */
+/*   Updated: 2020/04/05 17:04:30 by slabelle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+int		printf_unsigned_decimal(unsigned int ui_ap, t_flags *flags,
+		t_specifiers *specs)
+{
+	;
+}
