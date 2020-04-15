5288d59f7a0c5ed5e3ac7d88c263277760f7d6f6
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-15 16:57:13 +0200

    Update printf.h

--- printf.h
+++ printf.h
@@ -43,11 +43,12 @@ t_specifiers	*printf_init_specs(void);
 int				printf_width_precision(const char *format, va_list *ap,
 					t_flags *flags, t_specifiers *specs);
 
+int				printf_percent(t_flags *flags, t_specifiers *specs);
 int				printf_signed_decimal(int int_ap, t_flags *flags,
 					t_specifiers *specs);
-int				printf_unsigned_char(char char_ap, t_flags *flags,
-					t_specifiers *specs);
 int				printf_string(char *str_ap, t_flags *flags,
+					t_specifiers *specs);
+int				printf_unsigned_char(char char_ap, t_flags *flags,
 					t_specifiers *specs);
 
 #endif
