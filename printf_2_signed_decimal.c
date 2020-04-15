61bb4c8d0e399ebe90c5b8783179afbaadf46f4d
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-15 11:32:26 +0200

    Update printf_2_signed_decimal.c

--- printf_2_signed_decimal.c
+++ printf_2_signed_decimal.c
@@ -12,7 +12,7 @@
 
 #include "printf.h"
 
-static int	printf_display2_sd(int int_ap, t_specifiers *specs, char *str_ap,
+static int	printf_display2_sd(t_specifiers *specs, int int_ap, char *str_ap,
 			int ap_len)
 {
 	int		printed;
@@ -30,7 +30,7 @@ static int	printf_display2_sd(int int_ap, t_specifiers *specs, char *str_ap,
 	return (printed);
 }
 
-static int	printf_display_sd(int int_ap, t_flags *flags, t_specifiers *specs,
+static int	printf_display_sd(t_flags *flags, t_specifiers *specs, int int_ap,
 			char *str_ap)
 {
 	int		ap_len;
@@ -39,7 +39,7 @@ static int	printf_display_sd(int int_ap, t_flags *flags, t_specifiers *specs,
 	ap_len = (int)ft_strlen(str_ap);
 	printed = 0;
 	if (flags->minus == 1)
-		printed += printf_display2_sd(int_ap, specs, str_ap, ap_len);
+		printed += printf_display2_sd(specs, int_ap, str_ap, ap_len);
 	if (specs->precision >= 0 && specs->precision < ap_len)
 		specs->precision = ap_len;
 	if (specs->precision >= 0)
@@ -51,20 +51,22 @@ static int	printf_display_sd(int int_ap, t_flags *flags, t_specifiers *specs,
 		printed += printf_display_width_precision(specs->width, ap_len,
 			specs->precision);
 	if (flags->minus == 0)
-		printed += printf_display2_sd(int_ap, specs, str_ap, ap_len);
+		printed += printf_display2_sd(specs, int_ap, str_ap, ap_len);
 	return (printed);
 }
 
 int			printf_signed_decimal(int int_ap, t_flags *flags, t_specifiers *specs)
 {
 	int		printed;
+	int		int_cpy;
 	char	*str_ap;
 
 	if (int_ap == 0 && specs->precision == 0)
 		return (printf_display_width_precision(specs->width, 0, 0));
 	printed = 0;
+	int_cpy = int_ap;
 	if (int_ap < 0 && (specs->precision >= 0 || flags->zero == 1)
-		&& int_ap != -2147483648)
+		&& int_cpy != -2147483648)
 	{
 		if (specs->precision <= -1 && flags->zero == 1)
 			ft_putchar_fd('-', 1);
@@ -74,7 +76,7 @@ int			printf_signed_decimal(int int_ap, t_flags *flags, t_specifiers *specs)
 		printed++;
 	}
 	str_ap = ft_itoa(int_ap);
-	printed += printf_display_sd(int_ap, flags, specs, str_ap);
+	printed += printf_display_sd(flags, specs, int_cpy, str_ap);
 	free(str_ap);
 	return (printed);
 }
