9727fbcdfa30b8b933ce04f8292ee30b9f16c134
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-15 18:36:02 +0200

    Update printf_2_unsigned_hexa.c

---
+++ printf_2_unsigned_hexa.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_2_unsigned_hexa.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/05 16:31:24 by slabelle          #+#    #+#             */
+/*   Updated: 2020/04/05 17:04:30 by slabelle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+static int	printf_display2_uh(t_specifiers *specs, char *str_ap, int ap_len)
+{
+	int		printed;
+	int		k;
+
+	printed = 0;
+	if (specs->precision >= 0)
+		printed += printf_display_width_precision((specs->precision - 1),
+			(ap_len - 1), 1);
+	k = -1;
+	while (str_ap[++k] && k < ap_len)
+		printed += ft_putchar_fd(str_ap[k], 1);
+	return (printed);
+}
+
+static int	printf_display_uh(t_flags *flags, t_specifiers *specs, char *str_ap)
+{
+	int		ap_len;
+	int		printed;
+
+	ap_len = (int)ft_strlen(str_ap);
+	printed = 0;
+	if (flags->minus == 1)
+		printed += printf_display2_uh(specs, str_ap, ap_len);
+	if (specs->precision >= 0 && specs->precision < ap_len)
+		specs->precision = ap_len;
+	if (specs->precision >= 0)
+	{
+		specs->width -= specs->precision;
+		printed += printf_display_width_precision(specs->width, 0, 0);
+	}
+	else
+	{
+		if (specs->width && specs->precision == -1 && flags->zero == 0)
+			printed += printf_display_width_precision(specs->width, ap_len, 0);
+		else
+			printed += printf_display_width_precision(specs->width, ap_len,
+				specs->precision);
+	}
+	if (flags->minus == 0)
+		printed += printf_display2_uh(specs, str_ap, ap_len);
+	return (printed);
+}
+
+int			printf_unsigned_hexa(unsigned long long ull_ap, t_flags *flags,
+			t_specifiers *specs)
+{
+	int		printed;
+	char	*str_ap;
+	int		k;
+
+	if (ull_ap == 0 && specs->precision == 0)
+		return (printf_display_width_precision(specs->width, 0, 0));
+	printed = 0;
+	str_ap = ft_itoa_ull_base(ull_ap, 16);
+	if (width->type == 'X')
+	{
+		k = -1;
+		while (str_ap[++k])
+			str_ap[k] = ft_toupper(str_ap[k]);
+	}
+	printed += printf_display_uh(flags, specs, str_ap);
+	free(str_ap);
+	return (printed);
+}
