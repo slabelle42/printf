4e6e134c8aaa8d90ddc2252f111f75a4a9fa2591
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-16 11:26:34 +0200

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -12,57 +12,51 @@
 
 #include "printf.h"
 
-static int			printf_type(va_list *ap, t_flags *flags,
-					t_specifiers *specs)
+static int			printf_type(va_list *ap, t_specifiers *specs)
 {
 	int				printed;
 
 	printed = 0;
 	if (specs->type == 'c')
-		printed = printf_unsigned_char(va_arg(*ap, unsigned int), flags, specs);
+		printed = printf_unsigned_char(va_arg(*ap, unsigned int), specs);
 	else if (specs->type == 's')
-		printed = printf_string(va_arg(*ap, char *), flags, specs);
+		printed = printf_string(va_arg(*ap, char *), specs);
 	else if (specs->type == 'x' || specs->type == 'X')
-		printed = printf_unsigned_hexa(va_arg(*ap, unsigned long long),
-			flags, specs);
+		printed = printf_unsigned_hexa(va_arg(*ap, unsigned long long), specs);
 	else if (specs->type == 'd' || specs->type == 'i')
-		printed = printf_signed_decimal(va_arg(*ap, int), flags, specs);
+		printed = printf_signed_decimal(va_arg(*ap, int), specs);
 	else if (specs->type == 'u')
-		printed = printf_unsigned_decimal(va_arg(*ap, unsigned int), flags,
-			specs);
+		printed = printf_unsigned_decimal(va_arg(*ap, unsigned int), specs);
 	else if (specs->type == '%')
-		printed = printf_percent(flags, specs);
+		printed = printf_percent(specs);
 	return (printed);
 }
 
 static int			printf_parse(const char *format, va_list *ap,
-					t_flags *flags, t_specifiers *specs)
+					t_specifiers *specs)
 {
 	specs->j = specs->i;
 	while (format[++(specs->j)])
 		if (!(ft_strchr(".-0123456789*#+ ", format[specs->j])))
 			break ;
-	printf_flags(format, flags, specs);
-	if ((specs->width = printf_width_precision(format, ap,
-		flags, specs)) == -2)
+	printf_flags(format, specs);
+	if ((specs->width = printf_width_precision(format, ap, specs)) < 0)
 		exit(-1);
 	if (format[specs->i] == '.')
 	{
-		flags->dot = 1;
-		if ((specs->precision = printf_width_precision(format, ap,
-			flags, specs)) == -2)
+		specs->flag_dot = 1;
+		if ((specs->precision = printf_width_precision(format, ap, specs)) < -1)
 			exit(-1);
 	}
 	if (ft_strchr("cspxXdiu%", format[specs->i]))
 		specs->type = format[specs->i];
-	return (printf_type(ap, flags, specs));
+	return (printf_type(ap, specs));
 }
 
 static int			printf_input(const char *format, va_list *ap)
 {
 	int				i;
 	int				printed;
-	t_flags			*flags;
 	t_specifiers	*specs;
 
 	i = -1;
@@ -72,14 +66,11 @@ static int			printf_input(const char *format, va_list *ap)
 			printed += (int)ft_putchar_fd(format[i], 1);
 		else if (format[i] == '%' && format[i + 1])
 		{
-			if (!(flags = printf_init_flags()))
-				exit(-1);
-			if (!(specs = printf_init_specs()))
+			if (!(specs = printf_init_specifiers()))
 				exit(-1);
 			specs->i = i;
-			printed += printf_parse(format, ap, flags, specs);
+			printed += printf_parse(format, ap, specs);
 			i = specs->i;
-			free(flags);
 			free(specs);
 		}
 	return (printed);
