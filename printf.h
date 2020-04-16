90395a36c8fffc3eeba39e45f40aae5490d2b19a
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-16 11:30:00 +0200

    Update printf.h

--- printf.h
+++ printf.h
@@ -32,22 +32,18 @@ typedef struct	s_specifiers
 int				ft_printf(const char *format, ...);
 
 int				printf_display_width_precision(int field, int minus, int zero);
-void			printf_flags(const char *format, t_flags *flags,
-					t_specifiers *specs);
+void			printf_flags(const char *format, t_specifiers *specs);
 t_specifiers	*printf_init_specs(void);
 int				printf_width_precision(const char *format, va_list *ap,
-					t_flags *flags, t_specifiers *specs);
+					t_specifiers *specs);
 
-int				printf_percent(t_flags *flags, t_specifiers *specs);
-int				printf_signed_decimal(int int_ap, t_flags *flags,
+int				printf_percent(t_specifiers *specs);
+int				printf_signed_decimal(int int_ap, t_specifiers *specs);
+int				printf_string(char *str_ap, t_specifiers *specs);
+int				printf_unsigned_char(char char_ap, t_specifiers *specs);
+int				printf_unsigned_decimal(unsigned int uint_ap,
 					t_specifiers *specs);
-int				printf_string(char *str_ap, t_flags *flags,
-					t_specifiers *specs);
-int				printf_unsigned_char(char char_ap, t_flags *flags,
-					t_specifiers *specs);
-int				printf_unsigned_decimal(unsigned int uint_ap, t_flags *flags,
-					t_specifiers *specs);
-int				printf_unsigned_hexa(unsigned long long ull_ap, t_flags *flags,
+int				printf_unsigned_hexa(unsigned long long ull_ap,
 					t_specifiers *specs);
 
 #endif
