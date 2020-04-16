537d2db12ddc7ae4910648683c0679da8cb74553
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-04-16 16:01:09 +0200

    Update printf_2_pointer.c

--- printf_2_pointer.c
+++ printf_2_pointer.c
@@ -12,26 +12,45 @@
 
 #include "printf.h"
 
-static int	printf_display_pt(t_specifiers *specs, char *str_ap)
+static int	printf_display_pt(t_specifiers *specs, char *str_ap, int ap_len)
 {
-	;
+	int		printed;
+	int		k;
+
+	printed = 0;
+	printed += ft_putstr_fd("0x", 1);
+	k = -1;
+	if (specs->precision >= 0)
+	{
+		printed += printf_display_width_precision(specs->precision, ap_len, 1);
+		while (++k < specs->precision)
+			printed += ft_putchar_fd(str_ap[k]);
+	}
+	else
+		while (++k < ap_len)
+			printed += ft_putchar_fd(str_ap[k]);
+	return (printed);
 }
 
 int			printf_pointer(unsigned long long ull_ap, t_specifiers *specs)
 {
 	int		printed;
 	char	*str_ap;
+	int		ap_len;
 	int		k;
 
-	if (ull_ap == 0 && specs->precision == 0)
-	{
-		;
-	}
+//	if (ull_ap == 0 && specs->precision == 0)
+//		return ();
 	printed = 0;
 	str_ap = ft_itoa_ull_base(ull_ap, 16);
-	;
-	printed += printf_display_uh(specs, str_ap);
-	;
+	ap_len = (int)ft_strlen(str_ap);
+	if (specs->precision < ap_len)
+		specs->precision = ap_len;
+	if (specs->flag_minus == 1)
+		printed += printf_display_pt(specs, str_ap, ap_len);
+	printed += printf_display_width_precision(specs->width, (ap_len + 2), 0);
+	if (specs->flag_minus == 0)
+		printed += printf_display_pt(specs, str_ap, ap_len);
 	free(str_ap);
 	return (printed);
 }
